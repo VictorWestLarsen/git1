8b4ace992a2a89f2ce54d86d9f0b961fbb9e4ca3
Victor Wolden West-Larsen <vict536f@edu.eal.dk>
2018-09-24 11:10:36 +0200

    Vi præver igen på pc1

--- git1/Program.cs
+++ git1/Program.cs
@@ -11,10 +11,8 @@ namespace git1
         static void Main(string[] args)
         {
             Console.WriteLine("hello git1");
-            Console.WriteLine("Ny ændring");
-            Console.WriteLine("En ændring mere <3");
-            Console.WriteLine("Det er virker ikke");
-            Console.WriteLine("Ændring i git1 på pc2")
+            Console.WriteLine("Ny ændring på pc1");
+            Console.WriteLine("Vi prøver igen på pc1");
             Console.ReadLine();
 
         }
